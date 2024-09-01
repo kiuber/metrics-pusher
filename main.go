a639ad7974e3fd793131c3f47f4a52c54fcd1d86
kiuber <kiuber.zhang@gmail.com>
2024-09-01 10:35:06 +0800

    refactor: publish more functions

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-type Config struct {
+type PullPushConfig struct {
 	MetricsUrl          string `validate:"required" arg:"env:METRICS_URL" help:"metrics url" default:""`
 	PushgatewayUrl      string `validate:"required" arg:"env:PG_URL" help:"pushgateway url" default:""`
 	PushgatewayUsername string `arg:"env:PG_USERNAME" help:"pushgateway username" default:""`
@@ -21,43 +21,51 @@ type Config struct {
 }
 
 func main() {
-	config := &Config{}
+	config := &PullPushConfig{}
 	arg.MustParse(config)
 
-	if config.MetricsUrl != "" && config.PushgatewayUrl != "" {
-		c := cron.New(cron.WithSeconds(), cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)), cron.WithLogger(
-			cron.VerbosePrintfLogger(log.New(os.Stdout, "crontab: ", log.LstdFlags))), cron.WithLocation(time.UTC))
-
-		log.Printf("pushgateway crontab spec: %s", config.PushgatewayCrontab)
-		c.AddFunc(config.PushgatewayCrontab, func() {
-			log.Printf("Prepare push to %s", config.PushgatewayUrl)
-
-			resp, err := http.Get(config.MetricsUrl)
-			if err != nil {
-				fmt.Println("Error fetching metrics:", err)
-				return
-			}
-			if resp.StatusCode != http.StatusOK {
-				fmt.Println("Error fetching metrics:", resp.StatusCode)
-				return
-			}
-
-			defer resp.Body.Close()
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println("Error reading response body:", err)
-				return
-			}
-
-			Push(*config, body)
-		})
-		c.Start()
-	}
+	PullPushCrontab(*config)
 
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
 
-func Push(config Config, data []byte) {
+func PullPushCrontab(config PullPushConfig) {
+	if config.PushgatewayUrl != "" && config.MetricsUrl != "" {
+		c := cron.New(cron.WithSeconds(), cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)), cron.WithLogger(
+			cron.VerbosePrintfLogger(log.New(os.Stdout, "crontab: ", log.LstdFlags))), cron.WithLocation(time.UTC))
+
+		log.Printf("crontab spec: %s", config.PushgatewayCrontab)
+		c.AddFunc(config.PushgatewayCrontab, func() {
+			PullPush(config)
+		})
+		c.Start()
+	}
+}
+
+func PullPush(config PullPushConfig) {
+	log.Printf("Prepare pull %s push to %s", config.MetricsUrl, config.PushgatewayUrl)
+
+	resp, err := http.Get(config.MetricsUrl)
+	if err != nil {
+		fmt.Println("Error fetching metrics:", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error fetching metrics:", resp.StatusCode)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	Push(config, body)
+}
+
+func Push(config PullPushConfig, data []byte) {
 	log.Printf("Push to %s, username: %s", config.PushgatewayUrl, config.PushgatewayUsername)
 	req, err := http.NewRequest("POST", config.PushgatewayUrl, bytes.NewBuffer(data))
 	if err != nil {
